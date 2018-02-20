dbf482158a0db25a205dd074598139cd2c5e6c29
Gileno Filho <contato@gilenofilho.com.br>
2018-02-20 14:05:42 -0400

    adicionando exibição notícia

--- flask_examples/noticias/app.py
+++ flask_examples/noticias/app.py
@@ -1,17 +1,29 @@
+import dataset
+
 from flask import Flask, render_template
 
 app = Flask('noticias')
 
 
+def conexao():
+    return dataset.connect('sqlite:///noticias.db')
+
+
 @app.route('/')
 def index():
     titulo = 'Notícias'
-    noticias = [
-        'Notícia 1', 'Notícia 2', 'Notícia 3'
-    ]
+    tabela_noticias = conexao()['noticias']
+    noticias = tabela_noticias.all()
     return render_template(
         'index.html', titulo=titulo, noticias=noticias
     )
+
+
+@app.route('/noticias/<int:noticia_id>')
+def detalhes(noticia_id):
+    tabela_noticias = conexao()['noticias']
+    noticia = tabela_noticias.find_one(id=noticia_id)
+    return render_template('noticia.html', noticia=noticia)
 
 
 app.config['DEBUG'] = True
