6b4dada697237fedd49e5879302a532c6c19dd18
Gileno Filho <contato@gilenofilho.com.br>
2018-02-20 17:02:23 -0400

    adicionando modelo de Categoria

--- noticias_dj/core/models.py
+++ noticias_dj/core/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 
 
+class Categoria(models.Model):
+
+    titulo = models.CharField('Título', max_length=100)
+    criado_em = models.DateTimeField('Criado em', auto_now_add=True)
+    modificado_em = models.DateTimeField('Modificado em', auto_now=True)
+
+    def __str__(self):
+        return self.titulo
+    
+    class Meta:
+        db_table = 'categorias'
+
+
 class Noticia(models.Model):
 
     titulo = models.CharField('Título', max_length=100)
+    categoria = models.ForeignKey(
+        Categoria, models.SET_NULL, verbose_name='Categoria', null=True
+    )
     publicacao = models.DateField('Publicação', null=True, blank=True)
     conteudo = models.TextField('Conteúdo')
     criado_em = models.DateTimeField('Criado em', auto_now_add=True)
