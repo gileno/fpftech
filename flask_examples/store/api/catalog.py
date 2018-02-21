3b14db5160788f0cf17b6d5cc32540bb662a4393
Gileno Filho <contato@gilenofilho.com.br>
2018-02-21 10:19:26 -0400

    adicionando cliente produtos e api products

--- flask_examples/store/api/catalog.py
+++ flask_examples/store/api/catalog.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask.views import MethodView
 
+from ..extensions import db
 from ..models import Product, Category
 
 
@@ -14,11 +15,21 @@ class ProductAPI(MethodView):
             return jsonify(Product.query.filter_by(id=product_id).first().serialize)
         else:
             return jsonify(results=[p.serialize for p in Product.query.all()])
+    
+    def post(self):
+        data = request.form.copy()
+        product = Product()
+        product.name = data['name']
+        product.description = data['description']
+        product.category_id = data['category_id']
+        db.session.add(product)
+        db.session.commit()
+        return jsonify(product.serialize), 201
 
 
 product_view = ProductAPI.as_view('product_api')
 catalog_blueprint.add_url_rule(
-    '/products/', view_func=product_view, methods=['GET']
+    '/products/', view_func=product_view, methods=['GET', 'POST']
 )
 catalog_blueprint.add_url_rule(
     '/products/<int:product_id>', view_func=product_view, methods=['GET']
