58288b22029afca286c8790049520c3ae09d1af7
Gileno Filho <contato@gilenofilho.com.br>
2018-02-21 11:13:50 -0400

    adicionando cliente categorias

--- flask_examples/store/api/catalog.py
+++ flask_examples/store/api/catalog.py
@@ -8,6 +8,28 @@ from ..models import Product, Category
 catalog_blueprint = Blueprint('catalog', __name__)
 
 
+class CategoryAPI(MethodView):
+
+    def get(self, category_id=None):
+        if category_id:
+            return jsonify(Category.query.filter_by(id=category_id).one().serialize)
+        else:
+            q = request.args.get('q', '')
+            if q:
+                results = Category.query.filter(Category.name.contains(q))
+            else:
+                results = Category.query.all()
+            return jsonify(results=[c.serialize for c in results])
+    
+    def post(self):
+        data = request.form.copy()
+        category = Category()
+        category.name = data['name']
+        db.session.add(category)
+        db.session.commit()
+        return jsonify(category.serialize), 201
+
+
 class ProductAPI(MethodView):
 
     def get(self, product_id=None):
@@ -26,6 +48,15 @@ class ProductAPI(MethodView):
         db.session.commit()
         return jsonify(product.serialize), 201
 
+
+category_view = CategoryAPI.as_view('category_api')
+catalog_blueprint.add_url_rule(
+    '/categories/', view_func=category_view, methods=['GET', 'POST']
+)
+catalog_blueprint.add_url_rule(
+    '/categories/<int:category_id>', view_func=category_view,
+    methods=['GET']
+)
 
 product_view = ProductAPI.as_view('product_api')
 catalog_blueprint.add_url_rule(
